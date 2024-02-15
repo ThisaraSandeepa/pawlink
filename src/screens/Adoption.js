03885758ba9d1f1a33f88e699ff5759d37d28645
Thisara Sandeepa <thisarar46@gmail.com>
2024-02-15 23:55:34 +0530

    Edited the adoption

--- src/screens/Adoption.js
+++ src/screens/Adoption.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,Image, TouchableOpacity, GestureResponderEvent,Button,Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, GestureResponderEvent,Button,Image } from 'react-native';
 
 export default function Home() {
 
