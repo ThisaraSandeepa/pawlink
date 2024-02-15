2ffa09f0271170728d18c67a1d4bbb90d4ad0d65
Thisara Sandeepa <thisarar46@gmail.com>
2024-02-16 00:00:48 +0530

    Finalize the adoption section

--- src/screens/Adoption.js
+++ src/screens/Adoption.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput,Image, TouchableOpacity, GestureResponderEvent,Button,Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, GestureResponderEvent,Button,Image } from 'react-native';
 
 export default function Home() {
 
@@ -163,15 +163,14 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 5,
     top:20,
-    
   },
+
   buttonText: {
     color: 'white',
     textAlign: 'center',
     fontSize: 12,
     fontWeight: 'bold',
     fontFamily: 'Arial',
-
   },
 
 });
