ffbb3a696db286322526be0843ccd55c161f0e53
Abhiruwickz <136422566+Abhiruwickz@users.noreply.github.com>
2024-02-15 23:51:40 +0530

    css
    tailwind

--- src/screens/Adoption.js
+++ src/screens/Adoption.js
@@ -67,111 +67,14 @@ export default function Home() {
       <StatusBar style="auto" />
     </View>
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: '#888',
-    padding: 10,
-    marginBottom: 20,
-  },
-  searchBar: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: '#888',
-    padding: 10,
-    marginBottom: 20,
-    top:0,
-    borderRadius:10,
-    flexDirection:"row",
-  },
-  rectangleContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-    width: '100%',
-    padding: 10,
-  },
-  rectangle: {
-    width: '45%',
-    height: 180,
-    borderWidth: 1,
-    borderColor: '#888',
-    marginBottom: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-    resizeMode:'contain',
-    borderRadius:10,
-  },
-  additionalTextInput: {
-    width: '90%',
-    borderWidth: 1,
-    borderColor: '#888',
-    padding: 10,
-  },
-  image: {
-    width: '55%',
-    height: 150,
-    marginBottom: 20,
-    borderRadius: 10,
-    
-  },
-  rectangleimage: {
-    width: '100%',
-    height: 140,
-    marginBottom: 20,
-    borderRadius: 10,
-  },
-  checkrectangle: {
-    width: '80%',
-    height: 100,
-    borderWidth: 1,
-    borderColor: '#888',
-    marginBottom: 20,
-    backgroundColor:'#f6f7fb',
-    borderRadius:10,
-    top:0,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  checkrectangleimage:{
-    width: '100%',
-    height: 100,
-    borderRadius:10,
-  },
-
-  Text:{
-    fontSize: 10,
-    fontWeight: 'bold',
-  },
-  
-  Button: {
-    width: '20%',
-    backgroundColor: '#67bcd4',
-    height:35,
-    alignSelf:"center",
-    padding: 10,
-    borderRadius: 5,
-    top:20,
-    
-  },
-  buttonText: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 12,
-    fontWeight: 'bold',
-    fontFamily: 'Arial',
-
-  },
-
+  container: 'flex-1 bg-white items-center justify-center',
+  input: 'w-80 border-2 border-gray-800 p-4 mb-8',
+  searchBar: 'w-80 border-2 border-gray-800 p-4 mb-8 top-0 rounded flex-row',
+  rectangleContainer: 'flex-row flex-wrap',
 });
 
