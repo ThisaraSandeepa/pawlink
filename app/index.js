5abcbfdb2a8f9c29bbeffbf4766fc117513a9928
Vishmidulanjalee <135816897+Vishmidulanjalee@users.noreply.github.com>
2024-03-01 20:23:31 +0530

    Update index.js
    Did some changes on get started Page

--- app/index.js
+++ app/index.js
@@ -1,13 +1,40 @@
+import React from 'react';
 import { Link } from 'expo-router';
-import { View, Image } from 'react-native';
-
+import { View, Image,StyleSheet } from 'react-native';
 
 export default function index() {
   return (
-    <View className = "items-center">
-      <Image source={require("../assets/images/pawlink1.png")} className = "scale-50"/>
-      <Link href="./User/SignIn" className ="bg-blue-700 rounded text-white p-2"> Get Started </Link>
-    </View> 
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <Image
+        source={require("../assets/images/pawlink1.png")}
+        style={styles.image}
+      />
+      <Link
+        href="./User/SignIn"
+        style={{
+          backgroundColor: '#3252a8',
+          borderRadius: 8,
+          color: 'white',
+          paddingVertical:9,
+          paddingHorizontal: 20,
+          width: 170,
+          textAlign: 'center',
+          fontSize:20,
+         
+        }}
+      >
+        GET STARTED
+      </Link>
+      
+    </View>
   );
 }
-
+const styles = StyleSheet.create({
+  image: {
+    width: '80%', // Adjust the width as needed
+    height: '50%', // Adjust the height as needed
+    resizeMode: 'contain', // Adjust the resizeMode as needed
+    marginTop:-200,
+    marginBottom: 100, // Adjust the marginBottom to move the button closer
+  },
+});
