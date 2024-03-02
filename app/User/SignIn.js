79e24b6bff8f94b3adaa5310324c769305a75347
Vishmidulanjalee <135816897+Vishmidulanjalee@users.noreply.github.com>
2024-03-02 21:52:01 +0530

    Revert "Update SignIn.js"
    This reverts commit 608598b36579e26ddd0f933f8100ae58e33cfe62.

--- app/User/SignIn.js
+++ app/User/SignIn.js
@@ -1,95 +1,63 @@
 import React, { useState } from 'react';
-import { Link, useNavigation } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { signInWithEmailAndPassword } from 'firebase/auth';
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { FIREBASE_AUTH } from '../../FirebaseConfig';;
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const navigation = useNavigation(); // Assuming useNavigation is used from expo-router
 
   // Sign in with email and password
   const handleSignIn = async () => {
-    if (!email || !password) {
-      setEmailError(email ? '' : 'Required field');
-      setPasswordError(password ? '' : 'Required field');
-      return;
-    }
-
+    
     try {
       const userCredential = await signInWithEmailAndPassword(FIREBASE_AUTH, email, password);
-
+      
       // Signed in
       const user = userCredential.user;
       console.log("Logged In!");
-      navigation.replace('../SocialMedia/LandingPage'); // Fix the navigation method
+      router.replace('../SocialMedia/LandingPage');
 
     } catch (error) {
       alert(error.message);
     }
   };
 
-  const handleEmailChange = (email) => {
-    setEmail(email);
-    setEmailError(
-      email.trim() === '' // Check if the email field is empty
-        ? 'Required Field'
-        : !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) // Check if the email format is invalid
-        ? 'Invalid email format'
-        : '' // No error
-    );
-  };
-
-  const handlePasswordChange = (password) => {
-    setPassword(password);
-    setPasswordError(
-      password.trim() === '' // Check if the password field is empty
-        ? 'Required field'
-        : '' // No error
-    );
-  };
-
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', paddingTop: 36, gap: 24 }}>
-      <Text style={{ fontWeight: 'thin', fontSize: 60 }}> Welcome! </Text>
-      <View style={{ gap: 6, marginRight: 5, alignItems: 'center' }}>
+    <View className="justify-center items-center pt-36 gap-24">
+      <Text className="font-thin text-6xl"> Welcome! </Text>
+      <View className="gap-6 mr-5 items-center">
         <TextInput
           placeholder="Email"
-          style={{ border: 1, borderColor: '#888', borderRadius: 8, width: 300, padding: 10 }}
+          className="border border-gray-800 rounded w-72 p-2"
           autoCapitalize='none'
           value={email}
-          onChangeText={handleEmailChange}
+          onChangeText={text => setEmail(text)}
         />
-        {emailError ? <Text style={{ color: 'red' }}>{emailError}</Text> : null}
 
         <TextInput
           placeholder="Password"
           secureTextEntry
-          style={{ border: 1, borderColor: '#888', borderRadius: 8, width: 300, padding: 10 }}
+          className="border border-gray-800 rounded w-72 p-2"
           autoCapitalize='none'
           value={password}
-          onChangeText={handlePasswordChange}
+          onChangeText={text => setPassword(text)}
         />
-        {passwordError ? <Text style={{ color: 'red' }}>{passwordError}</Text> : null}
 
         <TouchableOpacity
-          style={{ backgroundColor: '#3252a8', borderRadius: 8, color: 'white', padding: 10, width: 120, textAlign: 'center' }}
-          onPress={handleSignIn}
-        >
-          <Text style={{ color: 'white', marginLeft: 2 }}> Log In </Text>
+          className="bg-blue-700 rounded text-white p-2 w-20 text-center"
+          onPress={handleSignIn}> 
+          <Text className = "text-white ml-2"> Log In </Text> 
         </TouchableOpacity>
-      </View>
 
-      <Link href="./SignUp" style={{ borderRadius: 8, color: '#3252a8', padding: 10, paddingTop: 24 }}>
+      </View>
+      <Link href="./SignUp" className="rounded text-blue-600 p-2 pt-24">
         Don't Have An Account?{' '}
-        <Text style={{ color: '#3252a8', fontWeight: 'bold' }}>Sign Up Now! </Text>
+        <Text className="text-blue-700 font-bold">Sign Up Now! </Text>
       </Link>
     </View>
   );
 };
 
 export default SignIn;
-
