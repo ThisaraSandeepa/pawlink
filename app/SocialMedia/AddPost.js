af993f6a7bc65b5ff464732f63fc0c76ea8b4e86
Asanga Gunawardhana <asangakavindi12@gmail.com>
2024-03-04 02:20:24 +0530

    Updated the Add post page with firebase

--- app/SocialMedia/AddPost.js
+++ app/SocialMedia/AddPost.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text, TouchableOpacity } from 'react-native';
+import { View, TextInput, Text, TouchableOpacity,Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import {getStorage,ref,uploadBytes,getDownloadURL} from 'firebase/storage';
+
+
+
 
 const AddPost = () => {
 
   // useState to store the image
   const [image, setImage] = useState(null);
+  const [uploading , setUploading] = useState(false);
+  const [transferred, settransferred] = useState(0);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -14,15 +20,43 @@ const AddPost = () => {
       aspect: [4, 3],
       quality: 1,
     });
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
   };
 
+  const submitImage = async () =>{
+    const uploadUrl = image;
+    let filename = uploadUri.substring(uploadUri.lastIndexOf('/')+1);
+
+    setUploading(true)
+
+
+    try{
+      await storage().ref(filename).putFile(uploadUri);
+
+      setUploading(false);
+      Alert.alert(  
+        'Image uploaded!',
+        'Your image has been uploaded to the Firebase Cloud Storage Successfully'
+      );
+      
+
+    }
+    catch(e){
+      console.log(e);
+    }
+
+    setImage(null);
+  }
   return (
     <View className = "my-auto">
       <View className = "items-center gap-3">
         <Text> Description </Text>
         <TextInput placeholder= "Type Here!" multiline={true} className = "border border-gray-800 rounded w-80 p-20"/>
         <TouchableOpacity 
-          onPress={pickImage}
+          onPress={submitImage}
           className="bg-blue-700 rounded  p-2 text-center">
           <Text className = "text-white"> Add Image </Text>
         </TouchableOpacity>
