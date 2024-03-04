7fe050d6309e8be32d552cccc054dba01b76713e
Abhiruwickz <136422566+Abhiruwickz@users.noreply.github.com>
2024-03-05 01:52:50 +0530

    connect firestore config for add post
    firestore database connected

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,13 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from  "firebase/auth";
 import { getStorage } from "firebase/storage"; 
 import { getFirestore } from "firebase/firestore";
-import firebase from 'firebase/compat/app';
-import'firebase/compat/storage';
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -21,13 +15,10 @@ const firebaseConfig = {
   appId: "1:919518194725:web:c2474f995950e20e3a23cc",
   measurementId: "G-5W6RXMBL4D"
 };
-if (!firebase.apps.length){
-  firebase.initializeApp(firebaseConfig);
-}
+
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
 export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP); 
 export const FIREBASE_STORAGE = getStorage(FIREBASE_APP);
-export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-export {firebase};
--- app/Adoption/AddStrayPost.js
+++ app/Adoption/AddStrayPost.js
@@ -1,116 +1,14 @@
-/*import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Alert, Image } from "react-native";
-import * as ImagePicker from 'expo-image-picker';
-import React, { useState } from "react";
-import * as FileSystem from 'expo-file-system';
-import { FIREBASE_APP } from '../../FirebaseConfig';
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-
-const dbStorage = getStorage(FIREBASE_APP);
-
-const UploadMediaFile = () => {
-    const [image, setImage] = useState(null);
-    const [uploading, setUploading] = useState(false);
-
-    const pickImage = async () => {
-        let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-        })
-        if (!result.cancelled) {
-            setImage(result.assets[0].uri)
-        }
-    };
-
-    const UploadMedia = async () => {
-        setUploading(true);
-
-        try {
-            const { uri } = await FileSystem.getInfoAsync(image);
-            const blob = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 });
-
-            const filename = image.substring(image.lastIndexOf('/') + 1);
-            const storageRef = ref(dbStorage, filename);
-
-            await uploadBytes(storageRef, blob, { contentType: 'image/jpeg/png ' });
-
-            setUploading(false);
-            Alert.alert('Photo Uploaded!!!');
-            setImage(null);
-
-        } catch (error) {
-            console.error(error);
-            setUploading(false);
-            Alert.alert('An error occurred while uploading the photo');
-        }
-    };
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <TouchableOpacity style={styles.selectButton} onPress={pickImage} >
-                <Text style={styles.buttonText}>Pick an image </Text>
-            </TouchableOpacity>
-            <View style={styles.imageContainer}>
-                {image && <Image
-                    source={{ uri: image }}
-                    style={{ width: 300, height: 300 }}
-                />}
-                <TouchableOpacity style={styles.uploadButton} onPress={UploadMedia} >
-                    <Text style={styles.buttonText}>Upload Image</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    )
-}
-
-export default UploadMediaFile;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    selectButton: {
-        borderRadius: 5,
-        width: 200,
-        height: 50,
-        backgroundColor: 'blue',
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    },
-    buttonText: {
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 18,
-    },
-
-    uploadButton: {
-        borderRadius: 5,
-        width: 200,
-        height: 50,
-        backgroundColor: 'red',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: 20,
-    },
-    imageContainer: {
-        marginTop: 30,
-        marginBottom: 50,
-        alignItems: 'center'
-    }
-});*/
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Alert, Image, TextInput, ScrollView } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
 import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getFirestore, addDoc,collection } from 'firebase/firestore';
 import { FIREBASE_APP } from '../../FirebaseConfig';
+import { router } from 'expo-router';
 
 const dbStorage = getStorage(FIREBASE_APP);
+const dbFirestore = getFirestore(FIREBASE_APP);
 
 const UploadMediaFile = () => {
     const [image, setImage] = useState(null);
@@ -120,7 +18,7 @@ const UploadMediaFile = () => {
     const [age, setAge] = useState("");
     const [color, setColor] = useState("");
     const [description, setDescription] = useState("");
-    const [automaticAgePrediction, setAutomaticAgePrediction] = useState(false);
+    //const [automaticAgePrediction, setAutomaticAgePrediction] = useState(false);
 
     const pickImage = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
@@ -143,19 +41,29 @@ const UploadMediaFile = () => {
 
             const filename = image.substring(image.lastIndexOf('/') + 1);
             const storageRef = ref(dbStorage, filename);
-
-            // Access contactInfo, location, color, description, and automaticAgePrediction and include them in the database/storage upload process
-
             await uploadBytes(storageRef, blob, { contentType: 'image/jpeg/png ' });
 
+            // Firestore data uploading
+            const newDocRef = await addDoc(collection(dbFirestore, "strayPosts"),{
+                contactInfo: contactInfo,
+                location: location,
+                age: age,
+                color: color,
+                description: description,
+                image: filename
+            });
+            console.log("Document written with ID: ", newDocRef.id);
+            
             setUploading(false);
             Alert.alert('Photo Uploaded!!!');
             setImage(null);
+            router.replace('./LandingPage');
 
         } catch (error) {
             console.error(error);
             setUploading(false);
             Alert.alert('An error occurred while uploading the photo');
+           
         }
     };
 
@@ -215,7 +123,7 @@ const styles = StyleSheet.create({
     scrollContainer: {
         alignItems: 'center',
         justifyContent: 'center',
-        paddingBottom: 20, // Adjust as needed
+        paddingBottom: 20, 
     },
     input: {
         height: 40,
@@ -224,15 +132,6 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         marginBottom: 10,
         paddingLeft: 10,
-    },
-    checkboxContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginBottom: 10,
-    },
-    checkboxLabel: {
-        marginLeft: 8,
-        fontSize: 16,
     },
     selectButton: {
         borderRadius: 5,
@@ -264,5 +163,3 @@ const styles = StyleSheet.create({
 });
 
 export default UploadMediaFile;
-
-
