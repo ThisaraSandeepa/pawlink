aa5d64d48e8cd2931d842d3e8abfdeb78325a9f7
Thisara Sandeepa <thisarar46@gmail.com>
2024-03-16 18:15:11 +0530

    Adjust the layouts and pass the parameters to the next page

--- app/Adoption/AdoptMe.js
+++ app/Adoption/AdoptMe.js
@@ -1,51 +1,61 @@
-import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
-import { useRoute } from '@react-navigation/native';
-import { Link } from 'expo-router';
-import { Icon } from 'react-native-elements';
+import React from "react";
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const AdoptMe = () => {
   const route = useRoute();
   const { post } = route.params;
-  console.log(post);
+
+  const navigation = useNavigation();
+  const goToDetails = () => {
+    navigation.navigate("components/Details", { post });
+  };
 
   return (
-
     <ScrollView>
       <View className="bg-white rounded text-white p-2 w-25 text-center">
-        <Image source={{ uri: post.imageURL }} className="mt-10 w-1/4 h-24 -top-2.5 rounded-lg" />
-         <View className="flex flex-row flex-wrap justify-between w-full p-10 border border-gray-400 shadow-lg h-96 bg-white mt-15 rounded-lg"> 
-
-          <View className="flex-row alignItems: 'center'," >
+        <Image
+          source={{ uri: post.image }}
+          className="mt-10 w-1/4 h-24 -top-2.5 rounded-lg"
+        />
+        <View className="flex flex-row flex-wrap justify-between w-full p-10 border border-gray-400 shadow-lg h-96 bg-white mt-15 rounded-lg">
+          <View className="flex-row ">
             <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-5">{post.color}</Text>
+              <Text className="text-blue-500 text-xs font-bold text-center -top-5">
+                {post.color}
+              </Text>
               <Text className="text-sm text-center -mt-5">Color</Text>
             </View>
             <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-5">{post.age}</Text>
+              <Text className="text-blue-500 text-xs font-bold text-center -top-5">
+                {post.age}
+              </Text>
               <Text className="text-sm text-center -mt-5">Age</Text>
             </View>
             <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-5">{post.breed}</Text>
+              <Text className="text-blue-500 text-xs font-bold text-center -top-5">
+                {post.breed}
+              </Text>
               <Text className="text-sm text-center -mt-5">Breed</Text>
             </View>
-
           </View>
           <View className="flex flex-col w-screen ">
             <View className="flex-row justify-center text-center ">
-              <Icon name='location' type='evilicon' color='red' size={30} />
-              <Text className="text-base font-bold text-justify">{post.location}</Text>
+              <Icon name="location" type="evilicon" color="red" size={30} />
+              <Text className="text-base font-bold text-justify">
+                {post.location}
+              </Text>
             </View>
             {/* <Text className="text-base font-bold text-justify top-2 mx-10">{post.description}</Text> */}
             {/* <Text className="text-base font-bold text-justify top-2 mx-10">{post.contactInfo}</Text> */}
           </View>
-          <Link
-            href="../components/Details"
-            className='bg-blue-800 rounded text-white p-2 w-25  mx-10 text-center flex  items-center'>
-            Adopt Me!
-          </Link>
-          </View> 
+          <TouchableOpacity className="bg-blue-800 rounded text-white p-2 w-25  mx-10 text-center flex  items-center" onPress={goToDetails}> 
+            <Text className="text-white text-center">Adopt Me</Text>
+          </TouchableOpacity>
         </View>
+      </View>
     </ScrollView>
   );
 };
