d472205bc5e3e9bd23aef00de4b383ebdc89eac9
Asanga Gunawardhana <asangakavindi12@gmail.com>
2024-03-18 02:33:11 +0530

    Change some css in AdoptMe.js
    change the width and height in rectangles, and front size

--- app/Adoption/AdoptMe.js
+++ app/Adoption/AdoptMe.js
@@ -28,37 +28,35 @@ const AdoptMe = () => {
         <View className="flex flex-row flex-wrap justify-between w-full p-10 border-3 border-gray-200 shadow-lg h-96 bg-white mt-15 rounded-md">
 
 
+
           <View className="flex-row items-center right-4">
-            <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-4">
+            <View className="w-2/5 h-28 w border-4 border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
+              <Text className="text-blue-500 text-base font-semibold text-center -top-4">
                 {post.color}
               </Text>
-              <Text className="text-sm text-center -mt-5">Color</Text>
+              <Text className=" text-center -mt-5 -my-1 text-lg font-bold">Color</Text>
             </View>
-            <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-4">
+            <View className="w-2/5 h-28 border-4 border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
+              <Text className="text-blue-500 text-base font-semibold text-center -top-4 ">
                 {post.age}
               </Text>
-              <Text className="text-sm text-center -mt-5">Age</Text>
+              <Text className=" text-center -mt-5 -my-1 text-lg font-bold">Age</Text>
             </View>
-            <View className="w-16 h-16 border border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
-              <Text className="text-blue-500 text-xs font-bold text-center -top-4">
-                {post.breed}
-              </Text>
-              <Text className="text-sm text-center -mt-5">Breed</Text>
-            </View>
+           
+              
+           
           </View>
-          <View className="flex flex-col  items-center justify-center mx-20 my-10 mt-2  ">
+          <View className="flex flex-col  items-center justify-center mx-10 my-10 -mt-12  left-8  ">
             <View className="flex-row justify-center text-center ">
               <Icon name="location" type="evilicon" color="red" size={30} />
-              <Text className="text-base font-bold text-justify">
+              <Text className="text-lg font-extrabold text-justify">
                 {post.location}
               </Text>
             </View>
             
           </View>
-          <TouchableOpacity className="bg-blue-800 rounded text-white p-2 w-25  mx-20 left-6 text-center flex  items-center" onPress={goToDetails}> 
-            <Text className="text-white text-center">Adopt Me</Text>
+          <TouchableOpacity className="bg-blue-800 rounded text-white p-4  w-25   mx-20 -right-1 text-center flex  items-center" onPress={goToDetails}> 
+            <Text className="text-white text-center text-base">Adopt Me</Text>
           </TouchableOpacity>
         </View>
       </View>
