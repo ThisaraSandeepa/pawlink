0346f9d2ec75ae178409aca2e48dd0a03f64b625
Asanga Gunawardhana <asangakavindi12@gmail.com>
2024-03-19 23:33:25 +0530

    Changed CSS in Details.js

--- app/components/Details.js
+++ app/components/Details.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Image, Text, ScrollView, TouchableOpacity } from "react-native";
+import {  View, Image, Text, ScrollView, TouchableOpacity } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 
 export default function AdoptionDetail() {
@@ -21,13 +21,16 @@ export default function AdoptionDetail() {
       <View className="flex-1 items-center justify-center ">
         <Image
           source={require("../../assets/images/pawlink1.png")}
-          style={styles.logo}
+          resizeMode= "contain"
+          className="w-116 h-[96px] top-0 mb-10"
         />
 
-        <Image source={{ uri: post.image }} style={styles.additionalImage} />
+        <Image source={{ uri: post.image }} 
+        resizeMode="cover"
+        className="flex-1 w-[307px] h-[273px] rounded-lg " />
 
-        <View style={styles.buttonContainer}>
-          <Text style={styles.Texts}>
+        <View className="flex-col items-center w-full p-10 border-gray-600  shadow-black h-[386px] bg-white mt-14 rounded-lg shadow-md">
+          <Text className="text-base text-black text-center mb-10 font-bold mt-20">
             Do you want to have veterinarian assistance before adopting
             procedure?
           </Text>
@@ -36,83 +39,16 @@ export default function AdoptionDetail() {
             onPress={goToVeterinarian}
             className="bg-blue-700 py-3 rounded-lg px-10 mt-5 mb-3"
           >
-            <Text style={styles.buttonText}>Yes I need</Text>
+            <Text className="text-white text-center text-base font-bold ">Yes I need</Text>
           </TouchableOpacity>
           <TouchableOpacity className="bg-red-700 py-3 rounded-lg px-10 mt-5 mb-3" onPress={goToIdentifier}>
-            <Text style={styles.buttonText}>No I don't</Text>
+            <Text className="text-white text-center text-base font-bold ">No I don't</Text>
           </TouchableOpacity>
         </View>
       </View>
     </ScrollView>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 116,
-    height: 96,
-    top: 0,
-    resizeMode: "contain",
-    marginBottom: 10,
-  },
 
-  additionalImage: {
-    flex: 1,
-    width: 307,
-    height: 273,
-    resizeMode: "cover",
-    borderRadius: 10,
-  },
-  Texts: {
-    fontSize: 16,
-    color: "black",
-    textAlign: "center",
-    marginBottom: 10,
-    fontWeight: "bold",
-    marginTop: 20,
-  },
 
-  buttonContainer: {
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    padding: 10,
-    borderColor: "#888",
-    shadowColor: "#000",
-    width: 362,
-    height: 386,
-    backgroundColor: "white",
-    marginTop: 15,
-    borderRadius: 10,
-    elevation: 4,
-  },
-  button1: {
-    width: 193,
-    height: 47,
-    borderRadius: 10,
-    backgroundColor: "blue",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 40,
-  },
-  button2: {
-    width: 193,
-    height: 47,
-    borderRadius: 10,
-    backgroundColor: "red",
-    justifyContent: "center",
-    alignItems: "center",
-    top: 80,
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-    fontSize: 15,
-    fontWeight: "bold",
-  },
-});
