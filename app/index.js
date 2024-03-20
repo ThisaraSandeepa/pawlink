4dcbfe299de87c318c8f5909d1c309fa18454c45
Abhiruwickz <136422566+Abhiruwickz@users.noreply.github.com>
2024-03-20 23:11:17 +0530

    logo rebuilding

--- app/index.js
+++ app/index.js
@@ -3,8 +3,8 @@ import { View, Image } from 'react-native';
 
 export default function index() {
   return (
-    <View className = "items-center">
-      <Image source={require("../assets/images/pawlink1.png")} className = "scale-50"/>
+    <View className = "items-center flex-1 mt-16">
+      <Image source={require("../assets/images/pawlink.png")} className = "scale-75 "/>
       <Link href="./User/SignIn" className ="bg-blue-700 rounded-lg text-white p-2 scale-125"> Get Started </Link>
     </View> 
   );
