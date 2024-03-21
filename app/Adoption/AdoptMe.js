ff6b50dd15a31e9598046622b8fc0a4488f1797f
Asanga Gunawardhana <asangakavindi12@gmail.com>
2024-03-21 11:21:37 +0530

    Update AdoptMe.js
    changed the size of image and add transition to button

--- app/Adoption/AdoptMe.js
+++ app/Adoption/AdoptMe.js
@@ -15,54 +15,52 @@ const AdoptMe = () => {
 
   return (
     <ScrollView>
-      <View className="bg-gray-150 rounded text-white p-2 text-center">
-        <View className="flex-row item ml-20 right-20 mb-3">
-          <Image
-            source={require("../../assets/images/pawlink1.png")}
-            className="w-1/4 h-24 mb-2 rounded-lg scale-75 "
-          />
-          <Text className="font-bold text-3xl text-blue-600 tracking-wider mt-8">
-            PawLink
-          </Text>
-        </View>
-
+      <View className="bg-white  text-white p-2 text-center rounded-lg  border-l border-white">
+        
         <Image
           source={{ uri: post.image }}
-          className="mt-4 w-1/2 h-48 -top-2.5 rounded-lg ml-20 left-4"
+          resizeMode="cover"
+          className="flex-1  h-[350px] rounded-t-3xl justify-center "
         />
-        <View className="flex flex-row flex-wrap justify-between w-full p-10 border-3 border-gray-200 shadow-lg h-96 bg-white mt-15 rounded-md">
+          
+        
+
+        
+        <View className="flex flex-row flex-wrap justify-between w-full p-10 border-3 border-gray-200 border-r-14 shadow-lg h-96 bg-white -mt-7 rounded-md rounded-t-3xl">
           <View className="flex-row items-center right-4">
             
-            <View className="w-2/5 h-28 w border-1 border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
+            <View className="w-2/5 h-28 w border border-gray-300 mb-20 mx-5 justify-center rounded-2xl shadow">
               <Text className="text-blue-500 text-base font-semibold text-center -top-4">
                 {post.color}
               </Text>
-              <Text className=" text-center -mt-5 -my-1 text-lg font-bold">
+              <Text className=" text-slate-500 text-center -mt-5 -my-1 text-lg font-bold">
                 Color
               </Text>
             </View>
-            <View className="w-2/5 h-28 border-1 border-gray-400 mb-20 mx-5 justify-center rounded-lg shadow">
+            <View className="w-2/5 h-28 border border-gray-300 mb-20 mx-5 justify-center rounded-2xl shadow">
               <Text className="text-blue-500 text-base font-semibold text-center -top-4 ">
                 {post.age}
               </Text>
-              <Text className=" text-center -mt-5 -my-1 text-lg font-bold">
+              <Text className="  text-slate-500 text-center -mt-5 -my-1 text-lg font-bold">
                 Age
               </Text>
             </View>
           </View>
-          <View className="flex flex-col  items-center justify-center mx-10 my-10 -mt-12  left-8  ">
+          <View className="flex flex-col  items-center  mx-10 px-14  -mt-12   ">
             <View className="flex-row justify-center text-center ">
               <Icon name="location" type="evilicon" color="red" size={30} />
-              <Text className="text-lg font-extrabold text-justify">
+              <Text className="text-slate-600 text-lg font-extrabold  text-center ">
                 {post.location}
               </Text>
             </View>
+            
           </View>
+          <Text className="  text-slate-500 text-gtext-base font-bold fixed text-base text-center left-10 top-2 mb-5 mx-10">{post.description}</Text> 
           <TouchableOpacity
-            className="bg-blue-800 rounded text-white p-2  w-25   mx-20 -right-1 text-center flex  items-center"
+            className="bg-blue-700 rounded-3xl text-white p-3  l mx-20  text-center  px-20 flex right-14 justify-items-center transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-gray-500 duration-400"
             onPress={goToDetails}
           >
-            <Text className="text-white text-center text-base">Adopt Me</Text>
+            <Text className="text-white text-center text-xl w-full ">Adopt Me</Text>
           </TouchableOpacity>
         </View>
       </View>
