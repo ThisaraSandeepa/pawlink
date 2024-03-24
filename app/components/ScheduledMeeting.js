31d4b5a7f7e6a775016ff1cf5a0fc7c5af80c5e8
Thisara Sandeepa <thisarar46@gmail.com>
2024-03-25 03:04:57 +0530

    Edited little bit of issues of the application

--- app/components/ScheduledMeeting.js
+++ app/components/ScheduledMeeting.js
@@ -1,26 +1,15 @@
 import React, { useState, useEffect } from "react";
-import {
-  
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  ScrollView,
-} from "react-native";
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native";
 import { Link } from "expo-router";
 import { useRoute } from "@react-navigation/native";
 import MapView, { Marker } from "react-native-maps";
 import { Icon } from "react-native-elements";
-
 
 // UserProfile Component
 export default function UserProfile() {
   const { post, slotData } = useRoute().params;
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
-
-  console.log(post);
-  console.log(slotData);
 
   useEffect(() => {
     const locationParts = post.locationKey.split("_");
@@ -31,12 +20,10 @@ export default function UserProfile() {
   }, []);
 
   return (
-  
-      <View className="flex-1 items-center mt-12  ">
-        {/* Profile Content */}
+    <View className="flex-1 items-center mt-12  ">
+      {/* Profile Content */}
 
-        
-        <View className="flex items-center h-screen w-full  bg-white shadow-black shadow-md ">
+      <View className="flex items-center h-screen w-full  bg-white shadow-black shadow-md ">
         <ScrollView>
           <View className="bg-white border border-blue-800 mt-12 w-[350px] h-[300px] flex items-center rounded-3xl">
             <Image
@@ -45,11 +32,18 @@ export default function UserProfile() {
             ></Image>
 
             <View className="flex flex-col items-center justify center -mt-1">
-              <Text className="font-extrabold text-lg">{slotData.veterinarian}</Text>
-              <Text className="font-bold text-base text-blue-900  ">Veterinarian</Text>
+              <Text className="font-extrabold text-lg">
+                {slotData.veterinarian}
+              </Text>
+              <Text className="font-bold text-base text-blue-900  ">
+                Veterinarian
+              </Text>
             </View>
 
-            <Text className="font-bold text-base items-center mb-3 mt-2">  Contact Number</Text>
+            <Text className="font-bold text-base items-center mb-3 mt-2">
+              {" "}
+              Contact Number
+            </Text>
             <View className="flex flex-col items-center -mt-1">
               <View className=" rounded-lg border-2  w-64 h-9  border-blue-400  ">
                 <Text className="text-center text-black font-semibold mt-1">
@@ -57,7 +51,10 @@ export default function UserProfile() {
                   {slotData.VeterinarianContact}{" "}
                 </Text>
               </View>
-              <Text className="font-bold text-base items-center mt-2 -mb-1"> Vet Location</Text>
+              <Text className="font-bold text-base items-center mt-2 -mb-1">
+                {" "}
+                Vet Location
+              </Text>
               <View className="flex flex-col items-center">
                 <View className=" rounded-lg border-2  w-64 h-9 border-blue-400 mt-2 ">
                   <Text className="text-center mt-1 text-black font-semibold  ">
@@ -71,8 +68,10 @@ export default function UserProfile() {
 
           <View className="bg-white border border-blue-800 mt-4 w-[350px] h-[600px] flex items-center rounded-3xl mb-6 ">
             <View className="mt-4 flex flex-col items-center justify center ">
-
-              <Text className="font-bold text-base items-center "> Identifier Contact Information</Text>
+              <Text className="font-bold text-base items-center ">
+                {" "}
+                Identifier Contact Information
+              </Text>
               <View className="flex flex-col items-center mt-2">
                 <View className=" rounded-lg border-2  w-64 h-9 border-blue-400">
                   <Text className="text-center mt-1 text-black font-semibold ">
@@ -81,10 +80,12 @@ export default function UserProfile() {
                   </Text>
                 </View>
               </View>
-
             </View>
             <View className="flex flex-col items-center mt-2">
-              <Text className="font-bold text-base items-center "> Identifier Name</Text>
+              <Text className="font-bold text-base items-center ">
+                {" "}
+                Identifier Name
+              </Text>
               <View className=" rounded-lg border-2  w-64 h-9 border-blue-400  mt-2">
                 <Text className="text-center mt-1 text-black font-semibold ">
                   {" "}
@@ -93,7 +94,9 @@ export default function UserProfile() {
               </View>
             </View>
             <View className="w-full flex items-center  mt-2">
-              <Text className="font-semibold text-black text-base items-center">Pickup Location</Text>
+              <Text className="font-semibold text-black text-base items-center">
+                Pickup Location
+              </Text>
             </View>
             <View className=" flex-row rounded-lg border-2 w-64 h-9  border-blue-400 mt-2 ">
               <Icon
@@ -103,7 +106,10 @@ export default function UserProfile() {
                 size={30}
                 className="mt-1 ml-6"
               />
-              <Text className="text-center mt-1 text-black font-semibold"> {post.location}</Text>
+              <Text className="text-center mt-1 text-black font-semibold">
+                {" "}
+                {post.location}
+              </Text>
             </View>
             {/* MapView */}
             <MapView
@@ -125,16 +131,15 @@ export default function UserProfile() {
             <View className="mt-4 flex justify-center items-center ">
               <TouchableOpacity className="flex justify-center items-center w-[200px] h-[40px] bg-blue-700 rounded-md mb-12">
                 <Link href={"../Adoption/LandingPage"}>
-                  <Text className="text-base font-extrabold text-white text-center leading-loose ">Return To Main Menu</Text>
+                  <Text className="text-base font-extrabold text-white text-center leading-loose ">
+                    Return To Main Menu
+                  </Text>
                 </Link>
               </TouchableOpacity>
             </View>
-
           </View>
-          </ScrollView>
-        </View>
-        
+        </ScrollView>
       </View>
-    
+    </View>
   );
 }
